5badb1336e856264a8cdf7ebae2534ae5ee3d2a2
RafaPaludo <rafael.ricardopp@gmail.com>
2025-12-10 21:01:51 -0300

    doc: adicionada jsdoc na função de debounce

--- app/composables/useDebouncedSaveAgenda.js
+++ app/composables/useDebouncedSaveAgenda.js
@@ -1,3 +1,13 @@
+/**
+ * 
+  Cria um debounce, após um determinado tempo a função é executada ou pode ser cancelada manualmente.
+  Útil para salvar automaticamente as ações do usuário sem sobrecarregar o banco.
+
+  @param callback - Função a ser executada
+  @param delay - Tempo que deve ser aguardado até executar a função novamente.
+  @returns {object.debouncedFn} - Função para executar o debounce
+  @returns {object.cancel} - Função para cancelar o debounce
+*/
 export function useDebounce(callback, delay = 500) {
   let timeout = null
 
